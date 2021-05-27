3dec9b80cbcbda33c90441a2f0e7042a36603c88
de313e <ufuk.altin@kiongroup.com>
2021-05-27 20:46:03 +0200

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+e
+5src/main/java/com/kion/ecom/products/Application.java,e\8\e8c1d53cdb6e850acf4a1dcfbcef0a7af6b054ac
+d
+4src/main/java/com/kion/ecom/products/model/Hero.java,c\3\c317fd50d61ce1fcefaa058f799818ffec06ab22
+m
+=src/main/java/com/kion/ecom/products/service/HeroService.java,a\a\aa436a7c2274fdb47c0433516b8db274fc3961a2
+s
+Csrc/main/java/com/kion/ecom/products/controller/HeroController.java,7\8\7871e7583ce759bc73b428f23aafe4523bdad263
---
+++ src/main/java/com/kism3t/productviewer/controller/HeroController.java
@@ -0,0 +1,43 @@
+package com.kism3t.productviewer.controller;
+
+import com.kism3t.productviewer.model.Hero;
+import com.kism3t.productviewer.service.HeroService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/heroes")
+public class HeroController {
+
+    private final HeroService heroService;
+
+    public HeroController(HeroService heroService) {
+        this.heroService = heroService;
+    }
+
+    @GetMapping
+    public Collection<Hero> getHeroes() {
+        return heroService.getHeroes().values();
+    }
+
+    @GetMapping("/{heroId}")
+    public Hero getHero(@PathVariable int heroId) {
+        return heroService.getHeroById(heroId);
+    }
+
+    @PutMapping()
+    public void updateHero(@RequestBody(required = true) Hero hero) {
+        heroService.updateHero(hero);
+    }
+
+    @PostMapping
+    public Hero createHero(@RequestBody(required = true) Hero hero) {
+        return heroService.createHero(hero);
+    }
+
+    @DeleteMapping("/{heroId}")
+    private Hero deleteHero(@PathVariable int heroId){
+        return heroService.deleteHero(heroId);
+    }
+}
